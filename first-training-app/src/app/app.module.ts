8270c08e8a4bc6549965b518a6cb73806a34cc58
Mathieu Enault <mathieu.enault@fabernovel.ey.com>
2024-10-11 11:42:44 +0200

    creating cards page

--- first-training-app/src/app/app.module.ts
+++ first-training-app/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { CardComponent } from './components/card/card.component';
 import { FormatDatePipe } from './pipe/date-pipe/date.pipe';
 import { ButtonComponent } from './common/followButton/follow-button.component';
+import { CardsComponent } from './pages/cards/cards.component';
 
 @NgModule({
-  declarations: [AppComponent, CardComponent, FormatDatePipe, ButtonComponent],
+  declarations: [AppComponent, CardComponent, FormatDatePipe, ButtonComponent, CardsComponent],
   imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent],
--- first-training-app/src/app/app.routes.ts
+++ first-training-app/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { CardComponent } from './components/card/card.component';
+import { CardsComponent } from './pages/cards/cards.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/card', pathMatch: 'full' },
+  { path: '', redirectTo: '/cards', pathMatch: 'full' },
+  { path: 'cards', component: CardsComponent },
   { path: 'card', component: CardComponent },
 ];
