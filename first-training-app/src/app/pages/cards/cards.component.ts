69cf96523313bd1fc9cabfee611575056eefec7d
Mathieu Enault <mathieu.enault@fabernovel.ey.com>
2024-10-11 15:10:42 +0200

    creating 2 areas of same width an cards page template and display an exemple of ordered list of followed users on left side

--- first-training-app/src/app/pages/cards/cards.component.ts
+++ first-training-app/src/app/pages/cards/cards.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'ns-cards',
   templateUrl: './cards.component.html',
-  styleUrls: ['./cards.component.css']
+  styleUrls: ['./cards.component.scss']
 })
 export class CardsComponent {
 
