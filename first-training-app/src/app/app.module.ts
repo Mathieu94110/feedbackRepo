f909922fcfd391b76c8fca8ac4041e7818a2fd4a
Mathieu Enault <mathieu.enault@fabernovel.ey.com>
2024-10-11 15:10:42 +0200

    Card page styling: split in 2

--- first-training-app/src/app/app.module.ts
+++ first-training-app/src/app/app.module.ts
@@ -7,9 +7,10 @@ import { CardComponent } from './components/card/card.component';
 import { FormatDatePipe } from './pipe/date-pipe/date.pipe';
 import { ButtonComponent } from './common/followButton/follow-button.component';
 import { CardsComponent } from './pages/cards/cards.component';
+import { FollowedUsersComponent } from './components/followed-users/followed-users.component';
 
 @NgModule({
-  declarations: [AppComponent, CardComponent, FormatDatePipe, ButtonComponent, CardsComponent],
+  declarations: [AppComponent, CardComponent, FormatDatePipe, ButtonComponent, CardsComponent, FollowedUsersComponent],
   imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent],
