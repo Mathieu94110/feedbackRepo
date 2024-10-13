581b1245178c5da01effe08c1d359549a23ee32d
Mathieu Enault <mathieu.enault@fabernovel.ey.com>
2024-10-13 19:40:51 +0200

    first five users now displayed within a card

--- first-training-app/src/app/components/card/card.component.ts
+++ first-training-app/src/app/components/card/card.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Steps } from 'src/models';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { IUserInfo, Steps } from 'src/models';
 
 @Component({
   selector: 'ns-card',
@@ -7,13 +7,28 @@ import { Steps } from 'src/models';
   styleUrls: ['./card.component.scss'],
 })
 export class CardComponent {
+  @Input() index!: number;
+  @Input() user!: IUserInfo;
+  @Input() indexOfFollowedUsers!: number[];
+  @Output() switchUserFollowed = new EventEmitter<{
+    user: IUserInfo;
+    index: number;
+  }>();
+
   date = new Date();
   currentStep: keyof typeof Steps = 'UnFollow';
   btnText: string = 'Follow';
+
   toggleOpen(): void {
     this.currentStep = 'Cancel';
   }
   changeBtnVal(value: string): void {
     this.btnText = value;
+  }
+  getFullName(firstname: string, lastname: string) {
+    return firstname + ' ' + lastname;
+  }
+  switchUserFollow() {
+    this.switchUserFollowed.emit();
   }
 }
