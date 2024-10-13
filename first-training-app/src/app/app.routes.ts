8d4edd1ae17b562a1de5d57fa2df57bca93ab0a1
Mathieu Enault <mathieu.enault@fabernovel.ey.com>
2024-10-13 20:35:29 +0200

    code cleaning

--- first-training-app/src/app/app.routes.ts
+++ first-training-app/src/app/app.routes.ts
@@ -1,9 +1,6 @@
 import { Routes } from '@angular/router';
-import { CardComponent } from './components/card/card.component';
 import { CardsComponent } from './pages/cards/cards.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/cards', pathMatch: 'full' },
-  { path: 'cards', component: CardsComponent },
-  { path: 'card', component: CardComponent },
+  { path: '', component: CardsComponent },
 ];
